d39c655ee7948e423d77f348566763bf2bce4da4
Daniel Ruiz <danirvicente@gmail.com>
2023-04-28 12:12:55 +0200

    First components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css"
+import { Home } from "./components/Home/Home"
+import { NavBar } from "./components/NavBar/NavBar"
+import { Footer } from "./components/Footer/Footer"
 
 function App() {
     return (
         <div className="app-container">
-            <h1>Gift Shop</h1>
+            <NavBar />
+            <Home />
+            <Footer />
         </div>
     )
 }
