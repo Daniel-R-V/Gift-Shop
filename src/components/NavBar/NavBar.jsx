4cd1bdb970d50ac86eee242436ab5e4a00408527
Daniel Ruiz <danirvicente@gmail.com>
2023-05-19 11:52:50 +0200

    cart improvements inside of the nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,10 +6,12 @@ import { CartContext } from "../../pages/ProductPage"
 import EmptyCart from "../EmptyCart/EmptyCart"
 import CartWithProducts from "../CartWithProducts/CartWithProducts"
 import { FaShoppingCart } from "react-icons/fa"
+import { RxHamburgerMenu } from "react-icons/rx"
 
 function Navbar() {
     const [sticky, setSticky] = useState(false)
     const [cart, setCart] = useState(false)
+    const [mobileNav, setMobileNav] = useState(false)
 
     const { cartProduct } = useContext(CartContext)
 
@@ -36,12 +38,28 @@ function Navbar() {
 
     return (
         <>
+            <div className={`mobile-nav-full ${mobileNav ? "open-flex" : "closed-flex"}`}>
+                {/* <i onClick={() => setMobileNav(!mobileNav)} className="fa-sharp fa-solid fa-xmark"></i> */}
+                <RxHamburgerMenu onClick={() => setMobileNav(!mobileNav)} />
+                <div className="mobile-links">
+                    <Link onClick={() => setMobileNav(!mobileNav)} to="/categories/all">
+                        Categories
+                    </Link>
+                    <Link onClick={() => setMobileNav(!mobileNav)} to="/categories/electronics">
+                        Electronics
+                    </Link>
+                    <Link onClick={() => setMobileNav(!mobileNav)} to="/categories/jewelery">
+                        Jewelery
+                    </Link>
+                </div>
+            </div>
+
             <div onClick={openCart} className={`page-overlay ${cart ? "open-flex" : "closed-flex"}`}></div>
 
             <div className={`cart-div ${cart ? "open-cart" : "closed-cart"}`}>
                 <div className="cart-title-btn">
                     <h2 className="cart-full-h2">Your Shopping Cart ({cartProduct.length})</h2>
-                    <FaShoppingCart className="simpleCart-icon" onClick={openCart} />
+                    <FaShoppingCart onClick={openCart} />
                 </div>
 
                 <div className="cart-body">{cartProduct.length < 1 ? <EmptyCart openCart={openCart} /> : <CartWithProducts />}</div>
@@ -69,6 +87,12 @@ function Navbar() {
                         <div className="auth-container">
                             <span className="login">Login</span>
                             <span className="sign-up">Sign Up</span>
+                        </div>
+                        <FaShoppingCart data-array-length={cartProduct.length} className={cartProduct.length < 1 ? "cart-icon" : "cart-icon with-items"} onClick={openCart} />
+
+                        <div className="hamburger-menu">
+                            <FaShoppingCart data-array-length={cartProduct.length} className={`fa-solid fa-cart-shopping hamburger-cart ${cartProduct.length < 1 ? "cart-icon" : "cart-icon with-items"}`} onClick={openCart} />
+                            <RxHamburgerMenu onClick={() => setMobileNav(!mobileNav)} />
                         </div>
                     </div>
                 </div>
